3b59c025066b10c82c618247a633e2b8839fe916
Gonzalo M <gonzalomoncadapaz@gmail.com>
2021-03-14 15:49:57 -0500

    ajustando mail y ico favicon

--- envio.php
+++ envio.php
@@ -1,16 +1,23 @@
 <?php
  
-if($_POST) {
+
     $nombre = $_POST['name'];
 	$email = $_POST['email'];
 	$mensaje = $_POST['message'];
     $destinatario="gmitiweb@gmail.com";
     $asunto="Contacto Syslab web";
+    $header="From: $email /r/n";
+    $header="To: $destinatario /r/n";
+    $header="X-Mailer: PHP/".phpversion();
 
     $carta="De: $nombre /n";
     $carta="Correo: $email /n";
     $carta="Mensaje: $mensaje";
 
-	mail($destinatario,$asunto,$carta);
+	$mail=mail($destinatario,$asunto,$carta,$header);
+    if ($mail) {
+        # code...
+        echo "<h3>Mail envio exitosamente</h3>";
+    }
  
 ?>
